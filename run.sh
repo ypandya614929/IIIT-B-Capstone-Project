27cb348d324b5668d8d62dbdc6e2dde6b4495120
Yash Pandya <ypandya614929@gmail.com>
2021-10-12 10:21:40 -0400

    Project implementation version-v4

--- run.sh
+++ run.sh
@@ -1,34 +1,34 @@
-#cd service-registry
-#sudo docker build -t bmc/service-registry:1.0.0 .
-#cd ..
-#
-#cd bmc-gateway
-#sudo docker build -t bmc/bmc-gateway:1.0.0 .
-#cd ..
-#
-#cd notification-service
-#sudo docker build -t bmc/notification-service:1.0.0 .
-#cd ..b
-#
-#cd doctor-service
-#sudo docker build -t bmc/doctor-service:1.0.0 .
-#cd ..
-#
-#cd user-service
-#sudo docker build -t bmc/user-service:1.0.0 .
-#cd ..
-#
-#cd appointment-service
-#sudo docker build -t bmc/appointment-service:1.0.0 .
-#cd ..
-#
-#cd payment-service
-#sudo docker build -t bmc/payment-service:1.0.0 .
-#cd ..
-#
-#cd rating-service
-#sudo docker build -t bmc/rating-service:1.0.0 .
-#cd ..
+cd service-registry
+sudo docker build -t bmc/service-registry:1.0.0 .
+cd ..
+
+cd bmc-gateway
+sudo docker build -t bmc/bmc-gateway:1.0.0 .
+cd ..
+
+cd notification-service
+sudo docker build -t bmc/notification-service:1.0.0 .
+cd ..b
+
+cd doctor-service
+sudo docker build -t bmc/doctor-service:1.0.0 .
+cd ..
+
+cd user-service
+sudo docker build -t bmc/user-service:1.0.0 .
+cd ..
+
+cd appointment-service
+sudo docker build -t bmc/appointment-service:1.0.0 .
+cd ..
+
+cd payment-service
+sudo docker build -t bmc/payment-service:1.0.0 .
+cd ..
+
+cd rating-service
+sudo docker build -t bmc/rating-service:1.0.0 .
+cd ..
 
 cd security-provider
 sudo docker build -t bmc/security-provider:1.0.0 .
